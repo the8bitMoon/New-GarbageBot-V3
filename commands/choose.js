0cdbe7c33307d551b316210eada91ca7f7007624
the8bitMoon <27790800+the8bitMoon@users.noreply.github.com>
2022-05-23 18:47:33 -0500

    Added first commands.

---
+++ commands/choose.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('choose')
+		.setDescription('Choose something from a list.')
+		.addStringOption((option) =>
+			option
+				.setName('list')
+				.setDescription('Enter a list of options separated by `|`.')
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		const list = interaction.options.getString('list').split(/\s*\|\s*/);
+		const choice = list[Math.floor(Math.random() * list.length)];
+		return interaction.reply({ content: `I choose ${choice}!` });
+	},
+};
---
+++ commands/echo.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Repeats your message back to you.')
+		.addStringOption((option) =>
+			option
+				.setName('text')
+				.setDescription('The message to repeat.')
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		const message = interaction.options.getString('text');
+		return interaction.reply(message);
+	},
+};
