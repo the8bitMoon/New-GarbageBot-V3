1206ed6b496ba023b696be79ba4c31a4fc1bc2aa
the8bitMoon <27790800+the8bitMoon@users.noreply.github.com>
2022-05-27 14:58:22 -0500

    Added tenor integration and updated kick command.

--- commands/kick.js
+++ commands/kick.js
@@ -1,20 +1,36 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment } = require('discord.js');
+const Tenor = require('tenorjs').client({
+	Key: process.env.tenorKey,
+	Filter: 'medium',
+	Locale: 'en_US',
+	MediaFilter: 'minimal',
+});
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kick')
-		.setDescription('Select a member and kick them (but not really).')
+		.setDescription('Kick someone!')
 		.addUserOption((option) =>
 			option
 				.setName('target')
-				.setDescription('The member to kick')
+				.setDescription('The member to kick.')
 				.setRequired(true),
 		),
 	async execute(interaction) {
-		const user = interaction.options.getUser('target');
-		return interaction.reply({
-			content: `You wanted to kick: ${user.username}`,
-			ephemeral: true,
+		const target = interaction.options.getUser('target');
+		const gif = await Tenor.Search.Random('anime kick', 1).then((Result) => {
+			return Result[0].media[0].tinygif.url;
+		});
+		console.log(gif);
+		const attachment = new MessageAttachment(gif, 'kick.gif', {
+			description: `${interaction.target} got kicked!`,
+		});
+
+		await interaction.deferReply();
+		return interaction.editReply({
+			content: `${interaction.user} kicks ${target}!`,
+			files: [attachment],
 		});
 	},
 };
