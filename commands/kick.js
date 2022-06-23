37109bb8af0d2efdbe1fc9677236073f19eb5438
the8bitMoon <27790800+the8bitMoon@users.noreply.github.com>
2022-06-23 09:52:05 -0500

    add context menus to anime gif commands

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,7 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const {
+	SlashCommandBuilder,
+	ContextMenuCommandBuilder,
+} = require('@discordjs/builders');
 const { MessageAttachment } = require('discord.js');
 
 module.exports = {
@@ -12,18 +15,29 @@ module.exports = {
 				.setRequired(true),
 		),
 	async execute(interaction) {
-		console.log(interaction.client.helpers.animeGif('kick'));
 		const target = interaction.options.getUser('target');
-		const gif = await interaction.client.helpers.animeGif('kick');
-		console.log(gif);
-		const attachment = new MessageAttachment(gif, 'kick.gif', {
-			description: `${interaction.target} got kicked!`,
-		});
-
-		await interaction.deferReply();
-		return interaction.editReply({
-			content: `${interaction.user} kicks ${target}!`,
-			files: [attachment],
-		});
+		return await run(interaction, target);
 	},
+};
+
+module.exports = {
+	data: new ContextMenuCommandBuilder().setName('kick').setType(2),
+	async execute(interaction) {
+		const target = interaction.targetUser;
+		return await run(interaction, target);
+	},
+};
+
+const run = async (interaction, target) => {
+	const gif = await interaction.client.helpers.animeGif('kick');
+	console.log(gif);
+	const attachment = new MessageAttachment(gif, 'kick.gif', {
+		description: `${interaction.target} got kicked!`,
+	});
+
+	await interaction.deferReply();
+	return interaction.editReply({
+		content: `${interaction.user} kicks ${target}!`,
+		files: [attachment],
+	});
 };
--- commands/slap.js
+++ commands/slap.js
@@ -1,4 +1,7 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const {
+	SlashCommandBuilder,
+	ContextMenuCommandBuilder,
+} = require('@discordjs/builders');
 const { MessageAttachment } = require('discord.js');
 
 module.exports = {
@@ -13,16 +16,28 @@ module.exports = {
 		),
 	async execute(interaction) {
 		const target = interaction.options.getUser('target');
-		const gif = await interaction.client.helpers.animeGif('slap');
-		console.log(gif);
-		const attachment = new MessageAttachment(gif, 'slap.gif', {
-			description: `${interaction.target} got slapped!`,
-		});
-
-		await interaction.deferReply();
-		return interaction.editReply({
-			content: `${interaction.user} slaps ${target}!`,
-			files: [attachment],
-		});
+		return await run(interaction, target);
 	},
+};
+
+module.exports = {
+	data: new ContextMenuCommandBuilder().setName('slap').setType(2),
+	async execute(interaction) {
+		const target = interaction.targetUser;
+		return await run(interaction, target);
+	},
+};
+
+const run = async (interaction, target) => {
+	const gif = await interaction.client.helpers.animeGif('slap');
+	console.log(gif);
+	const attachment = new MessageAttachment(gif, 'slap.gif', {
+		description: `${interaction.target} got slapped!`,
+	});
+
+	await interaction.deferReply();
+	return interaction.editReply({
+		content: `${interaction.user} slaps ${target}!`,
+		files: [attachment],
+	});
 };
