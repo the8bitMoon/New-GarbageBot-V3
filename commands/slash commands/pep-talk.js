90d8f30ba2679064b2444ffa54933c068e309462
the8bitMoon <27790800+the8bitMoon@users.noreply.github.com>
2022-12-01 16:41:39 -0600

    Add Anton font, fix peptalk text formatting issue

--- commands/slash commands/pep-talk.js
+++ commands/slash commands/pep-talk.js
@@ -6,12 +6,14 @@ const { fetch } = require('undici');
 const Canvas = require('@napi-rs/canvas');
 const sizeOf = require('image-size');
 const { MessageAttachment } = require('discord.js');
+const path = require('node:path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pep-talk')
 		.setDescription('Get some encouragement from an anime girl!'),
 	async execute(interaction) {
+		const imgWidth = 600;
 		const pepTalks = {
 			col1: [
 				'Champ,',
@@ -113,28 +115,43 @@ module.exports = {
 		const img = await interaction.client.helpers.getImageBuffer(url);
 		const waifuDimensions = sizeOf(img);
 
-		// Create the canvas with a width of 600px and keeping image aspect ratio.
+		// Create the canvas with fixed width, keeping aspect ratio.
 		const canvas = Canvas.createCanvas(
-			600,
-			(600 / waifuDimensions.width) * waifuDimensions.height,
+			imgWidth,
+			(imgWidth / waifuDimensions.width) * waifuDimensions.height,
 		);
 		const ctx = canvas.getContext('2d');
 		const bg = await Canvas.loadImage(img);
 		ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 
+		// Get the font.
+		Canvas.GlobalFonts.registerFromPath(
+			path.join(__dirname, '..', '..', 'assets', 'Anton-Regular.ttf'),
+			'Anton',
+		);
+
+		// Find the correct font size
+		let fontSize = 70;
+		do {
+			ctx.font = `${(fontSize -= 5)}px Anton`;
+			console.log(ctx.font);
+		} while (
+			ctx.measureText(line1).width > canvas.width - 20 ||
+			ctx.measureText(line2).width > canvas.width - 20
+		);
+
 		// Text formatting.
 		ctx.strokeStyle = '#000';
 		ctx.lineWidth = 8;
 		ctx.fillStyle = '#fff';
-		ctx.font = `bold 60px sans-serif`;
-		ctx.textAlign = 'left';
+		ctx.textAlign = 'center';
 
 		// Draw the text.
-		ctx.strokeText(line1, 40, canvas.height - 120, canvas.width - 40);
-		ctx.strokeText(line2, 40, canvas.height - 50, canvas.width - 40);
+		ctx.strokeText(line1, canvas.width / 2, canvas.height - fontSize * 2.5);
+		ctx.strokeText(line2, canvas.width / 2, canvas.height - fontSize * 1.25);
 
-		ctx.fillText(line1, 40, canvas.height - 120, canvas.width - 40);
-		ctx.fillText(line2, 40, canvas.height - 50, canvas.width - 40);
+		ctx.fillText(line1, canvas.width / 2, canvas.height - fontSize * 2.5);
+		ctx.fillText(line2, canvas.width / 2, canvas.height - fontSize * 1.25);
 
 		const attachment = new MessageAttachment(
 			await canvas.encode('jpeg'),
